ea1cb444345590482181b040b735c8bc8968b73a
Admin <Admin@HOME>
2022-10-28 10:45:37 +0530

    MyFirstCommitandPush

---
+++ HelloWorldGit/src/com/home/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.home.helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World, Welcome to my GitHub Program");
+	}
+
+}
